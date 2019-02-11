4311c75e966897be0c4a3cc1f26f4b600dc4f9da
helicopark <helicopark92@gmail.com>
2019-02-11 16:40:52 +0900

    - first commit

---
+++ app/src/main/java/com/helicopark/softcamera/MainActivity.java
@@ -0,0 +1,13 @@
+package com.helicopark.softcamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
